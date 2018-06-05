7bddf314ff23aedf5c9271954c8e4130037507a6
root <root@kdecifap001l-0.fjnx>
2018-06-05 12:13:37 +0800

    support jdk 1.5

--- src/test/java/com/jakewharton/fliptables/util/AbstractResultSetMetaData.java
+++ src/test/java/com/jakewharton/fliptables/util/AbstractResultSetMetaData.java
@@ -13,101 +13,101 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.jakewharton.fliptables.util;
+package com.jakewharton.fliptables.util;
 
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public abstract class AbstractResultSetMetaData implements ResultSetMetaData {
-  @Override public int getColumnCount() throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int getColumnCount() throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isAutoIncrement(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isAutoIncrement(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isCaseSensitive(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isCaseSensitive(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isSearchable(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isSearchable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isCurrency(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isCurrency(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public int isNullable(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int isNullable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isSigned(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isSigned(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public int getColumnDisplaySize(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int getColumnDisplaySize(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getColumnLabel(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getColumnLabel(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getColumnName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getColumnName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getSchemaName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getSchemaName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public int getPrecision(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int getPrecision(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public int getScale(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int getScale(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getTableName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getTableName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getCatalogName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getCatalogName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public int getColumnType(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public int getColumnType(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getColumnTypeName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getColumnTypeName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isReadOnly(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isReadOnly(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isWritable(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isWritable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isDefinitelyWritable(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isDefinitelyWritable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public String getColumnClassName(int column) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public String getColumnClassName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public <T> T unwrap(Class<T> iface) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override public boolean isWrapperFor(Class<?> iface) throws SQLException {
-    throw new UnsupportedOperationException();
-  }
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 }
